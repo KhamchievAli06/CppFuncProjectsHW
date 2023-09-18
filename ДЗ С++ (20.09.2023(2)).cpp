488c4ac02a8fb0a17e09e582ea94f916e438a4f2
KhamchievAli06 <144795257+KhamchievAli06@users.noreply.github.com>
2023-09-18 09:57:05 +0300

    Add files via upload
    ДЗ С++ (20.09.2023(2))

---
+++ ДЗ С++ (20.09.2023(2)).cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "");
+	float a1;
+	cout << "Введите оценку за первую чертветь: ";
+	cin >> a1;
+	float a2;
+	cout << "Введите оценку за вторую чертветь: ";
+	cin >> a2;
+	float a3;
+	cout << "Введите оценку за третия чертветь: ";
+	cin >> a3;
+	float a4;
+	cout << "Введите оценку за четвёртую чертветь: ";
+	cin >> a4;
+	cout << "Годовая оценка: " << round((a1 + a2 + a3 + a4) / 4);
+}
