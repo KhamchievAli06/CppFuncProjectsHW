c666518800d066ee56b8237aa022dbf49592faeb
KhamchievAli06 <144795257+KhamchievAli06@users.noreply.github.com>
2023-09-18 10:11:33 +0300

    Add files via upload
    ДЗ С++ (20.09.2023(3))

---
+++ ДЗ С++ (20.09.2023(3)).cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "");
+	float a1;
+	cout << "Введите зарплату за первый месяц: ";
+	cin >> a1;
+	float a2;
+	cout << "Введите зарплату за первый месяц: ";
+	cin >> a2;
+	float a3;
+	cout << "Введите зарплату за первый месяц: ";
+	cin >> a3;
+	cout << "Итоговая зарплата за квартал: " << a1 + a2 + a3;
+}
