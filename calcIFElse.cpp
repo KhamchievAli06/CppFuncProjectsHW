4013412b2b2cb5835b4a19fe8d96664b596fe584
KhamchievAli06 <144795257+KhamchievAli06@users.noreply.github.com>
2023-09-19 14:42:12 +0300

    creat else (if).Cpp

---
+++ calcIFElse.cpp
@@ -0,0 +1,35 @@
+﻿#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "");
+
+	//calculator (only output result + - * /
+	float a, b;
+	char op;
+	cout << "Input a ";
+	cin >> a;
+	cout << "Input b ";
+	cin >> b;
+	cout << "Выберите действие: +,-,:,* ";
+	cin >> op;
+	if (op == '+'){
+		cout << (a + b);
+	}
+	else if (op == '-') {
+		cout << (a - b);
+	}
+
+
+	
+
+
+	
+
+	
+
+
+	
+}
