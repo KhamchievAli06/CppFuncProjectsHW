8561aeb0f63d6a67cb2184249c5664cfb4ca397a
KhamchievAli06 <144795257+KhamchievAli06@users.noreply.github.com>
2023-09-23 00:17:45 +0300

    Add files via upload
    ДЗ С++ (27.09.2023(2))

---
+++ ДЗ(27.09.2023(2)).cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream> 
+using namespace std;
+int main() {
+    setlocale(LC_ALL, "");
+    int a, b, c, d, i;
+    cout << "Введите четырёхзначное число: ";
+    cin >> a;
+    b = a / 1000;
+    c = a / 100 % 10;
+    d = a / 10 % 10;
+    i = a % 10;
+    if (a < 999 or a > 9999) {
+        cout << "Введено не четырёхзначное число";
+    }
+    else {
+        cout << c * 1000 + b * 100 + i * 10 + d;
+    }
+}
