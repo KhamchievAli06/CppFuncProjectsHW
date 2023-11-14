dd68ecfac41d0f64745b6bcf55fc526e2ab1b884
KhamchievAli06 <144795257+KhamchievAli06@users.noreply.github.com>
2023-11-14 11:42:00 +0300

    Update dz(20.11.2023).cpp

--- dz(20.11.2023).cpp
+++ dz(20.11.2023).cpp
@@ -16,3 +16,28 @@ int main() {
 	}
 	cout << "\n" << "мин = " << min << "; макс = " << max << ";";
 }
+
+
+#include <iostream>
+using namespace std;
+int main() {
+	setlocale(LC_ALL, "");
+	srand(time(NULL));
+	int array[10];
+	int min, max, summ = 0;
+	cout << "Введите диапазон: ";
+	cin >> min >> max;
+	int a1;
+	cout << "Введите любое число из диапазона: ";
+	cin >> a1;
+	if (a1 < min || a1 > max) cout << "некорректно введено число";
+	for (int i = 0; i < 10; i++) {
+		int r = min + rand() % (max - 1 + min);
+		array[i] = r;
+		cout << array[i] << " ";
+		if (r <= a1) summ += r;
+	}
+	cout << summ;
+}
+
+
